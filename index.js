e8bfda00b248e7ec779605d6aedf393a46a19d02
Kim, Allen <allen.kim@ontariohealth.ca>
2023-08-27 08:48:34 -0400

    updated

--- index.js
+++ index.js
@@ -54,6 +54,126 @@ Util.DEBUG && console.log('  *** config ***', config);
 
 Util.FRAMEWORK = config.framework || argv.framework;
 
+function run (tsFile) {
+  try {
+    const testGenerator = getTestGenerator(tsFile, config);
+    const typescript = fs.readFileSync(path.resolve(tsFile), 'utf8');
+    const angularType = Util.getAngularType(typescript).toLowerCase();
+    const {ejsData} = testGenerator.getData();
+
+    ejsData.config = config;
+    // mockData is set after each statement is being analyzed from getFuncMockData
+    ejsData.ctorParamJs; // declarition only, will be set from mockData
+    ejsData.providerMocks; //  declarition only, will be set from mockData
+    ejsData.accessorTests = {}; //  declarition only, will be set from mockData
+    ejsData.functionTests = {}; //  declarition only, will be set from mockData
+
+    const Klass = getKlass(typescript, config);
+
+    Util.DEBUG &&
+      console.warn('\x1b[36m%s\x1b[0m', `PROCESSING ${Klass.ctor && Klass.ctor.name} constructor`);
+    const ctorMockData = getFuncMockData(Klass, 'constructor', 'constructor');
+
+    const ctorParamJs = Util.getFuncParamJS(ctorMockData.params);
+    ejsData.ctorParamJs = Util.indent(ctorParamJs, ' '.repeat(6)).trim();
+    ejsData.providerMocks = testGenerator.getProviderMocks(ctorMockData.params);
+
+    const errors = [];
+    testGenerator.klassSetters.forEach(setter => {
+      const setterName = setter.node.name.escapedText;
+      ejsData.accessorTests[`${setterName} SetterDeclaration`] =
+        Util.indent(getFuncTest(Klass, setterName, 'set', angularType), '  ');
+    });
+    testGenerator.klassGetters.forEach(getter => {
+      const getterName = getter.node.name.escapedText;
+      ejsData.accessorTests[`${getterName} GetterDeclaration`] =
+        Util.indent(getFuncTest(Klass, getterName, 'get', angularType), '  ');
+    });
+
+    testGenerator.klassMethods.forEach(method => {
+      const methodName = method.node.name.escapedText;
+      try {
+        ejsData.functionTests[methodName] =
+          Util.indent(getFuncTest(Klass, methodName, 'method', angularType), '  ');
+      } catch (e) {
+        const msg = '    // '+ e.stack;
+        const itBlock = `it('should run #${method.name}()', async () => {\n` +
+          `${msg.replace(/\n/g, '\n    // ')}\n` +
+          `  });\n`
+        ejsData.functionTests[methodName] = itBlock;
+        errors.push(e);
+      }
+    });
+
+    const generated = testGenerator.getGenerated(ejsData, argv);
+    generated && testGenerator.writeGenerated(generated, argv);
+
+    errors.forEach( e => console.error(e) );
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+const isDir = fs.lstatSync(tsFile).isDirectory();
+if (isDir) {
+  const files = glob.sync('**/!(*.spec).ts', {cwd: tsFile})
+  files.forEach(file => {
+    const includeMatch = config.includeMatch.map(re => file.match(re)).some(e => !!e);
+    const excludeMatch = config.excludeMatch.map(re => file.match(re)).some(e => !!e);
+    if (excludeMatch) {
+      console.log(' *** NOT processing (in excludeMatch)', path.join(tsFile, file));
+    } else if (includeMatch) {
+      console.log(' *** processing', path.join(tsFile, file));
+      run(path.join(tsFile, file));
+    } else {
+      console.log(' *** NOT processing (not in includeMatch)', path.join(tsFile, file));
+    }
+  });
+} else {
+  run(tsFile);
+}
+
+function getKlass(typescript, config) {
+  let replacedTypescript = 
+    typescript.match(/class .*?{.*}$/ms)[0]
+      .replace(/\s+extends\s\S+ {/gm, ' extends Object {') // rchange inheritance to an Object
+
+  config.replacements.forEach( ({from,to}) => {
+    replacedTypescript = replacedTypescript.replace(new RegExp(from, 'gm'), to);
+  })
+
+  const result = ts.transpileModule(replacedTypescript, {
+    compilerOptions: {
+      module: ts.ModuleKind.CommonJS,
+      experimentalDecorators: true,
+      removeComments: true,
+      target: ts.ScriptTarget.ES2015
+    }
+  });
+
+  // console.log('-----------------replacedTypescript--------------------');
+  // console.log(replacedTypescript);
+  // console.log('-----------------result.outputText --------------------');
+  // console.log(result.outputText);
+  // console.log('-------------------------------------');
+
+  let klassText = result.outputText
+    .replace(/super\(.*\);/gm, '') // remove inheritance code.
+    .replace(/super\./gm, 'this.'); // change inheritance call to this call
+  klassText =
+    klassText.match(/(class .*?{.*});?\n.*?__decorate/ms)?.[1] || /* with decorators */
+    klassText.match(/(class .*?{.*});?$/ms)?.[1] || /* generic */
+    klassText;
+
+  // console.log('---------------- klassText ---------------------');
+  // console.log(klassText);
+  // console.log('-------------------------------------');
+
+  const Klass = new Function(`return ${klassText}`)();
+  return Klass;
+}
+
 function loadConfig(filePath) {
   const userConfig = require(filePath);
   for (var key in userConfig) {
@@ -118,112 +238,4 @@ function getFuncTest(Klass, funcName, funcType, angularType) {
       ${funcAssertJS.join(';\n')}${funcAssertJS.length ? ';' : ''}
     });
     `;
-}
-
-function run (tsFile) {
-  try {
-    const testGenerator = getTestGenerator(tsFile, config);
-    const typescript = fs.readFileSync(path.resolve(tsFile), 'utf8');
-    const angularType = Util.getAngularType(typescript).toLowerCase();
-    const {ejsData} = testGenerator.getData();
-
-    ejsData.config = config;
-    // mockData is set after each statement is being analyzed from getFuncMockData
-    ejsData.ctorParamJs; // declarition only, will be set from mockData
-    ejsData.providerMocks; //  declarition only, will be set from mockData
-    ejsData.accessorTests = {}; //  declarition only, will be set from mockData
-    ejsData.functionTests = {}; //  declarition only, will be set from mockData
-
-    const result = ts.transpileModule(typescript, {
-      compilerOptions: {
-        module: ts.ModuleKind.CommonJS,
-        experimentalDecorators: true,
-        removeComments: true,
-        target: ts.ScriptTarget.ES2015
-      }
-    });
-
-    const klassText = (
-      result.outputText.match(/(class .*?{.*});\n.*?__decorate/ms) || /* Angular syntax */
-      result.outputText.match(/(class .*?{.*})\nexports/ms) /* generic syntax */
-    )[1];
-
-    let replacedOutputText = klassText
-      .replace(/super\(.*\);/gm, '') // remove inheritance code
-      .replace(/super\./gm, 'this.') // change inheritance call to this call
-      .replace(/\s+extends\s\S+ {/gm, ' extends Object {') // rchange inheritance to an Object
-    config.replacements.forEach( ({from,to}) => {
-      replacedOutputText = replacedOutputText.replace(new RegExp(from, 'gm'), to);
-    })
-
-    const Klass = new Function(`return ${replacedOutputText}`)();
-
-    Util.DEBUG &&
-      console.warn('\x1b[36m%s\x1b[0m', `PROCESSING ${Klass.ctor && Klass.ctor.name} constructor`);
-    const ctorMockData = getFuncMockData(Klass, 'constructor', 'constructor');
-
-    const ctorParamJs = Util.getFuncParamJS(ctorMockData.params);
-    ejsData.ctorParamJs = Util.indent(ctorParamJs, ' '.repeat(6)).trim();
-    ejsData.providerMocks = testGenerator.getProviderMocks(ctorMockData.params);
-    // for (var key in ejsData.providerMocks) {
-    //   ejsData.providerMocks[key] = Util.indent(ejsData.providerMocks[key]).replace(/\{\s+\}/gm, '{}');
-    // }
-
-    const errors = [];
-    testGenerator.klassSetters.forEach(setter => {
-      const setterName = setter.node.name.escapedText;
-      ejsData.accessorTests[`${setterName} SetterDeclaration`] =
-        Util.indent(getFuncTest(Klass, setterName, 'set', angularType), '  ');
-    });
-    testGenerator.klassGetters.forEach(getter => {
-      const getterName = getter.node.name.escapedText;
-      ejsData.accessorTests[`${getterName} GetterDeclaration`] =
-        Util.indent(getFuncTest(Klass, getterName, 'get', angularType), '  ');
-    });
-
-    testGenerator.klassMethods.forEach(method => {
-      const methodName = method.node.name.escapedText;
-      try {
-        ejsData.functionTests[methodName] =
-          Util.indent(getFuncTest(Klass, methodName, 'method', angularType), '  ');
-      } catch (e) {
-        const msg = '    // '+ e.stack;
-        const itBlock = `it('should run #${method.name}()', async () => {\n` +
-          `${msg.replace(/\n/g, '\n    // ')}\n` +
-          `  });\n`
-        ejsData.functionTests[methodName] = itBlock;
-        errors.push(e);
-      }
-    });
-
-    // console.log('..................................................................')
-    // console.log(ejsData)
-    // console.log('..................................................................')
-    const generated = testGenerator.getGenerated(ejsData, argv);
-    generated && testGenerator.writeGenerated(generated, argv);
-
-    errors.forEach( e => console.error(e) );
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-}
-
-const isDir = fs.lstatSync(tsFile).isDirectory();
-if (isDir) {
-  const files = glob.sync('**/!(*.spec).ts', {cwd: tsFile})
-  files.forEach(file => {
-    const includeMatch = config.includeMatch.map(re => file.match(re)).some(e => !!e);
-    const excludeMatch = config.excludeMatch.map(re => file.match(re)).some(e => !!e);
-    if (excludeMatch) {
-      console.log(' *** NOT processing (in excludeMatch)', path.join(tsFile, file));
-    } else if (includeMatch) {
-      console.log(' *** processing', path.join(tsFile, file));
-      run(path.join(tsFile, file));
-    } else {
-      console.log(' *** NOT processing (not in includeMatch)', path.join(tsFile, file));
-    }
-  });
-} else {
-  run(tsFile);
 }
