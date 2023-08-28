b60338552df862e64f78c071e0784c9761640fbd
Kim, Allen <allen.kim@ontariohealth.ca>
2023-08-28 17:10:04 -0400

    vercel test 1

--- api/index.js
+++ api/index.js
@@ -7,17 +7,21 @@
 // Response: {
 //   output: string // e.g. '// @ts-nocheck\n import { async, ComponentFixture, TestBed } ...'
 // }
+/**
+ * this express server on Vercel requires vercel.json
+ * {
+ *   "rewrites": [{ "source": "/api/(.*)", "destination": "/api" }]
+ * }
+ */
 const app = require('express')();
-const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(`Hello! ngentest on Vercel`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
+app.get('/api/ngentest', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
