2fa31f026e3fec41a541ff461b65f8b25783ac3f
Kim, Allen <allen.kim@ontariohealth.ca>
2023-08-28 17:54:18 -0400

    updated

--- api/index.js
+++ api/index.js
@@ -14,7 +14,25 @@ app.use(express.json());
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Welcome to ngentest@${package.version} on Vercel`);
+  res.end(`
+    <h1>Welcome to ngentest@${package.version} on Vercel</h1>
+    <b>Usage Example</b>
+    <pre>
+      fetch('/api/ngentest', {
+        method: 'POST',
+        headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          typescript: 
+            "@Component({selector: 'app-root'})" +
+            "class MyTestComponent {}",
+          options: {
+            tsPath: './my.test.component.ts'
+          } 
+        })
+       }).then(resp => resp.json())
+       .then(resp => console.log(resp.output))
+     </pre>
+  `);
 });
 
 /**
@@ -45,19 +63,6 @@ app.post('/api/ngentest', (req, res) => {
   const {typescript, options = {}} = req.body;
   const output = ngentest(typescript, options);
   res.send({output})
-});
-
-app.get('/api/ngentest', (req, res) => {
-  const typescript = `
-    @Component({
-      selector: 'app-root',
-      template: '<div>Example Component</div>',
-      styles: [''],
-      providers: [FooKlass],
-      x: {foo:1, bar:2}
-    })
-    class MyTestComponent {}`;
-  res.end(ngentest(typescript, {tsPath: './my-test.component.ts'}));
 });
 
 module.exports = app;
