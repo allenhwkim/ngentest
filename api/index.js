cde1560e8142cb130ad1c7e46122ebde509e3e58
Kim, Allen <allen.kim@ontariohealth.ca>
2023-08-28 17:45:36 -0400

    added post method

--- api/index.js
+++ api/index.js
@@ -1,12 +1,3 @@
-// TODO: ongoing work to run this as an API
-// POST /api/gentest
-// Payload {
-//   typescript: 'xxxxxxxxxxxxxxxxxx',
-//   options: e.g. {framework: 'jest', templates: {......}, directives: {.....}}
-// }
-// Response: {
-//   output: string // e.g. '// @ts-nocheck\n import { async, ComponentFixture, TestBed } ...'
-// }
 /**
  * this express server on Vercel requires vercel.json
  * {
@@ -15,11 +6,44 @@
  */
 const app = require('express')();
 const ngentest = require('../src/index.js');
+const package = require('../package.json');
+
+app.use(express.json());
 
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! ngentest on Vercel`);
+  res.end(`Welcome to ngentest@${package.version} on Vercel`);
+});
+
+/**
+ * fetch('/api/ngentest', {
+ *   method: 'POST',
+ *   headers: {
+ *     'Accept': 'application/json',
+ *     'Content-Type': 'application/json'
+ *   },
+ *   body: JSON.stringify({
+ *     typescript: `
+ *       @Component({
+ *         selector: 'app-root',
+ *         template: '<div>Example Component</div>',
+ *         styles: [''],
+ *         providers: [FooKlass],
+ *         x: {foo:1, bar:2}
+ *       })
+ *       class MyTestComponent {}`,
+ *       options: { 
+ *         tsPath: './my-test.component.ts' 
+ *       }
+ *     })
+ * }).then(resp => resp.json())
+ * .then(resp => console.log(resp))
+ */
+app.post('/api/ngentest', (req, res) => {
+  const {typescript, options = {}} = req.body;
+  const output = ngentest(typescript, options);
+  res.send({output})
 });
 
 app.get('/api/ngentest', (req, res) => {
